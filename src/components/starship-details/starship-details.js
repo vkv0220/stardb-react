01adaafbd60401a827d2c67eb310531f2347e429
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2019-11-09 18:37:19 +0700

    Components layout

---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './starship-details.css';
+
+const StarshipDetails = () => {
+  return(
+    <div className="starship-details">
+
+    </div>
+  );
+};
+
+export default StarshipDetails;
